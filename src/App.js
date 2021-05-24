70a39ba54740f569943ac72421b39576044f73e9
Evandro Carreira <evandro.carreira@gmail.com>
2021-05-24 19:12:40 -0300

    feat(map): load data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
 import useViewport from './components/useViewport'
 import KeplerGl from 'kepler.gl';
+import { addDataToMap } from 'kepler.gl/actions';
+import { useEffect } from 'react';
+import keplergljson from './geojson/keplergl.json'
+import { useDispatch } from 'react-redux';
+import {processKeplerglJSON} from 'kepler.gl/processors';
 
 const token = process.env.REACT_APP_MAPBOX_PUBLIC_TOKEN
 
 function App() {
   const { width, height } = useViewport();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(addDataToMap(processKeplerglJSON(keplergljson)));
+  }, [dispatch])
+
   return (
     <div className='App'>
       <KeplerGl
