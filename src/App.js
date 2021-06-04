c8d08e90d0c8eaf0232f883761601edd499c0ba4
Evandro Carreira <evandro.carreira@gmail.com>
2021-06-04 20:25:52 -0300

    refactor(p5): Remove kepler, mappa with p5.js

--- src/App.js
+++ src/App.js
@@ -1,39 +1,54 @@
 import './App.css';
 import useViewport from './components/useViewport'
-import KeplerGl from 'kepler.gl';
-import { addDataToMap } from 'kepler.gl/actions';
 import { useEffect } from 'react';
-import keplergljson from './geojson/keplergl.json'
 import { useDispatch } from 'react-redux';
-import {processKeplerglJSON} from 'kepler.gl/processors';
-import * as Tone from 'tone'
+import Sketch from 'react-p5';
+import Mappa from 'mappa-mundi'
 
 const token = process.env.REACT_APP_MAPBOX_PUBLIC_TOKEN
+// const mappa = new Mappa('Mapboxgl', token);
+const mappa = new Mappa('Leaflet');
 
+const options = {
+  lat: 0,
+  lng: 0,
+  zoom: 4,
+  style: "http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+}
+
+let myMap
 function App() {
   const { width, height } = useViewport();
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(addDataToMap(processKeplerglJSON(keplergljson)));
   }, [dispatch])
 
   useEffect(() => {
-    console.log('tone mount')
-    const osc = new Tone.Oscillator().toDestination();
-    osc.frequency.value = "C4";
-    osc.frequency.rampTo("G4", 2);
-    osc.start().stop("+5");
   }, [])
 
+  const setup = (p5, canvasParentRef) => {
+    
+		// use parent to render the canvas in this ref
+		// (without that p5 will render the canvas outside of your component)
+		const canvas = p5.createCanvas(width-10, height-10).parent(canvasParentRef);
+    myMap = mappa.tileMap(options); // lat 0, lng 0, zoom 4
+    myMap.overlay(canvas)
+	};
+
+	const draw = (p5) => {
+    const nigeria = myMap.latLngToPixel(11.396396, 5.076543); 
+    // Using that position, draw an ellipse
+    p5.ellipse(nigeria.x, nigeria.y, 200, 200);
+		p5.ellipse(20, 20, 70, 70);
+		// NOTE: Do not use setState in the draw function or in functions that are executed
+		// in the draw function...
+		// please use normal variables or class properties for these purposes
+	};
+  
   return (
     <div className='App'>
-      <KeplerGl
-        id='mapa'
-        mapboxApiAccessToken={token}
-        width={width}
-        height={height}
-      />
+      <Sketch setup={setup} draw={draw} />;
     </div>
   )
 }
