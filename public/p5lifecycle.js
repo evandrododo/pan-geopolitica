c35454bde3fa37401575203e3b49fedaac9874f4
Evandro Carreira <evandro.carreira@gmail.com>
2021-06-05 11:53:14 -0300

    fix(p5) sound map

--- public/p5lifecycle.js
+++ public/p5lifecycle.js
@@ -4,6 +4,11 @@ let time = 0
 let year = 1980
 let playing = false
 
+let attackTime;
+let l1 = 0.7; // attack level 0.0 to 1.0
+let t2 = 0.3; // decay time in seconds
+let l2 = 0.1; // decay level  0.0 to 1.0
+let l3 = 0.2; // release time in seconds
 // Carrega mapa
 const options = {
   lat: -23,
@@ -15,14 +20,11 @@ const options = {
 const token = 'pk.eyJ1IjoiZXZhbmRyb2RvZG8iLCJhIjoidlVvRHpXNCJ9.84ZWNOAqO60Smpy2Z1tFtg'
 const mappa = new Mappa("Mapbox", token)
 
-let notes = [60, 62, 64, 67, 69, 72]
-
-
 function setup() {
   canvas = createCanvas(window.innerWidth, window.innerHeight)
   myMap = mappa.tileMap(options) // lat 0, lng 0, zoom 4
   myMap.overlay(canvas)
-  polySynth = new p5.PolySynth()
+  env = new p5.Envelope();
   
   // Carrega infos
   fetch("./workdata.geojson")
@@ -41,16 +43,13 @@ function setup() {
         })
       })
     })
-    console.log('work', workdata)
 
-  //the looper's callback is passed the timeFromNow
-   //this value should be used as a reference point from
-   //which to schedule sounds
-   let intervalInSeconds = 0.2;
-   soundLoop = new p5.SoundLoop(onSoundLoop, intervalInSeconds);
-
-   synth = new p5.MonoSynth();
    sineOsc = new p5.Oscillator('sine');
+   triOsc = new p5.TriOsc();
+   sineOsc.freq(110);
+   triOsc.freq(220);
+   triOsc.amp(0.5);
+   sineOsc.amp(0.5);
    canvas.mousePressed(playOscillator);
 }
 
@@ -62,13 +61,7 @@ function draw() {
     if(year > 2020) year = 1990
     console.log('year', year)
   }
-  if(notes.length  > 0) {
-    // soundLoop.start()
-  } else {
-    soundLoop.stop()
-  }
   clear()
-  notes = []
 
   workdata.forEach(country => {
     const { coordinates } = country
@@ -78,28 +71,26 @@ function draw() {
     stroke(0,0,0,0)
     fill(255,255,5,200)
     ellipse(x, y, radius, radius)
+
+    // Caso o país esteja visível
     if(x > 0 && x < width && y > 0 && y < height && radius > 0) {
-      notes.push(radius)
-      sineOsc.freq(radius, 0.1);
-      sineOsc.amp(0.5);
+      // notes.push(radius)
+      triOsc.amp(radius);
+      sineOsc.amp(radius);
     }
+
   })
   textSize(64)
   fill(255)
   text(year, window.width - 180, window.height - 50);
 }
 
-function onSoundLoop(timeFromNow) {
-  let noteIndex = (soundLoop.iterations - 1) % notes.length;
-  let note = midiToFreq(notes[noteIndex]);
-  synth.play(note, 0.5, timeFromNow);
-  background(noteIndex * 360 / notes.length, 50, 100);
-}
-
 function playOscillator() {
   // starting an oscillator on a user gesture will enable audio
   // in browsers that have a strict autoplay policy.
   // See also: userStartAudio();
+  env.set(attackTime, l1, t2, l2, l3)
   sineOsc.start();
-  playing = true;
+  triOsc.start();
+  // env.play(triOsc)
 }
